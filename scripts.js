b86d6e8d40da5b0cadd533b99f27c1fef55435c7
stu <stu@BUILD-WI15-389.local>
2017-02-23 16:27:29 -0600

    updated index.html and styles.css created script.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function createX() {
+    var para = document.createElement("P");
+    var t = document.createTextNode("X");
+    t.id = 'draggable';
+    para.appendChild(t);
+    document.body.appendChild(para);
+}
+    
+function createO() {
+    var para = document.createElement("P");
+    var t = document.createTextNode("O");
+    t.id = 'draggable';
+    para.appendChild(t);
+    document.body.appendChild(para);
+}
+
